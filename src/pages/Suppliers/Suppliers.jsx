2e9cabba29a337f99887d58a6f2d1403b610782c
JavierValenzuelaEsparza <javier.valenzuela@g-global.com>
2024-11-14 11:57:03 -0800

    Change the services

--- src/pages/Suppliers/Suppliers.jsx
+++ src/pages/Suppliers/Suppliers.jsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import SuppliersTable from './suppliersTable';
+import RestockProductsPage from './RestockProductsPage';
 
 const Suppliers = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<SuppliersTable />} />
+        <Route path="/request-restock" element={<RestockProductsPage/>}/>
       </Routes>
     </div>
   );
