7a1a89a77f8e193b8bdf28834db23c1dc7e8f2d4
jaispuro27 <jjoseaispuro27@gmail.com>
2024-11-15 22:10:00 -0800

    CRUD

--- src/services/services.jsx
+++ src/services/services.jsx
@@ -13,13 +13,41 @@ export const fetchData = async (endpoint) => {
 };
 
 export const fetchDataTesting = async (endpoint) => {
-    try {
-        console.log(endpoint)
-        const response = await axios.get(`${endpoint}`);
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        throw error;
-    }
+  try {
+    console.log(endpoint);
+    const response = await axios.get(`${endpoint}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
 };
 
+export const postData = async (endpoint, data) => {
+  try {
+    const response = await axios.post(`${endpoint}`, data, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error posting data:', error);
+    throw error;
+  }
+};
+
+export const putData = async (endpoint, data) => {
+  try {
+    const response = await axios.put(`${endpoint}`, data, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error updating data:', error);
+    throw error;
+  }
+};
+y
