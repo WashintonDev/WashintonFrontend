66d4417628276c6d01569c2cff6cf4f9b485791b
JavierValenzuelaEsparza <javier.valenzuela@g-global.com>
2024-11-19 00:50:35 -0800

    resolve conflicts

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,9 +1,7 @@
-// src/routes/AppRoutes.jsx
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProtectedRoute from '../components/protectedRoutes';
-import Login from '../Login/Login';
+import Login from '../pages/Login/Login';
 import Home from '../pages/Home/Home';
 import Suppliers from '../pages/Suppliers/Suppliers';
 import SignUp from '../pages/Login/Sign-up';
