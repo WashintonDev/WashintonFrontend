54c9cb1cb508422967fb2060a3760400ac521767
JavierValenzuelaEsparza <javier.valenzuela@g-global.com>
2024-11-19 00:46:58 -0800

    resolve conflicts

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,13 +1,14 @@
+// src/routes/AppRoutes.jsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProtectedRoute from '../components/protectedRoutes';
-import Login from '../pages/Login/Login.jsx';
+import Login from '../pages/Login/Login';
 import Home from '../pages/Home/Home';
 import Suppliers from '../pages/Suppliers/Suppliers';
 import SignUp from '../pages/Login/Sign-up';
 import Inventory from '../pages/Inventory/Inventory';
 import Sales from '../pages/Sales/Sales';
-import TransferOrders from '../pages/TransferOrders/TransferOrders'
+import TransferOrders from '../pages/TransferOrders/TransferOrders';
 import Dispatch from '../pages/Dispatch/dispatch';
 import Admin from '../pages/Dashboard/Admin';
 import Profile from '../pages/Profile/Profile';
@@ -16,7 +17,6 @@ import UserManagement from '../pages/Dashboard/UserManagement';
 const AppRoutes = () => (
   <Router>
     <Routes>
-      
       <Route path="/login" element={<Login />} />
       <Route element={<ProtectedRoute />}>
         <Route path="/" element={<Home />} />
@@ -25,14 +25,10 @@ const AppRoutes = () => (
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sales/*" element={<Sales />} />
         <Route path="/dispatch/*" element={<Dispatch />} />
-        <Route path="/profile" element={<Profile />} /> 
+        <Route path="/profile" element={<Profile />} />
         <Route path="/transfer-orders/*" element={<TransferOrders />} />
-        {/* Administrativas */}
         <Route path="/Admin/*" element={<Admin />} />
         <Route path="/admin/users" element={<UserManagement />} />
-
-
-  
       </Route>
     </Routes>
   </Router>
