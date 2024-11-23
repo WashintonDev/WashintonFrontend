957c242a796b93413821464e0420e1d94510354a
AlexisPN <0321101363@ut-tijuana.edu.mx>
2024-11-22 17:00:15 -0800

    Añadir menú lateral con nuevas secciones y mejorar la estructura del componente NavBar

--- src/components/NavBar Home.jsx
+++ src/components/NavBar Home.jsx
@@ -3,7 +3,8 @@ import { Dropdown, Menu, Button, Input, Badge, Layout } from 'antd';
 import {
     UserOutlined, LogoutOutlined, FundProjectionScreenOutlined, SolutionOutlined,
     FormOutlined, ShoppingOutlined, MenuFoldOutlined, ExclamationCircleOutlined,
-    TruckOutlined, FundOutlined
+    TruckOutlined, FundOutlined,
+    SendOutlined
 } from '@ant-design/icons';
 import { signOut } from "firebase/auth";
 import { auth } from '../services/firebaseConfig';
@@ -37,104 +38,165 @@ const NavBarHome = ({ onSearch, showSearch = true }) => {
     );
 
     return (
-        <Layout>
-            <Sider
-                collapsible
-                collapsed={collapsed}
-                onCollapse={setCollapsed}
-                width={250}
-                style={{ height: '100vh', position: 'fixed', left: 0  }}
-                expandIcon = {null}
-                
+      <Layout>
+        <Sider
+          collapsible
+          collapsed={collapsed}
+          onCollapse={setCollapsed}
+          width={250}
+          style={{ height: "100vh", position: "fixed", left: 0 }}
+          expandIcon={null}
+        >
+          <Menu
+            mode="inline"
+            style={{ height: "100%", borderRight: 50, paddingTop: "50px" }}
+            defaultOpenKeys={["accountSettings"]}
+            expandIcon={null}
+          >
+            <Menu.Item key="dashboard" icon={<FundProjectionScreenOutlined />}>
+              <Link to="/dashboard">Dashboard</Link>
+            </Menu.Item>
+
+            <Menu.Item key="orders" icon={<SolutionOutlined />}>
+              <Link to="/orders">Orders</Link>
+            </Menu.Item>
+
+            <Menu.Item key="reports" icon={<FormOutlined />}>
+              <Link to="/reports">Reports</Link>
+            </Menu.Item>
+
+            <Menu.SubMenu key="sales" icon={<ShoppingOutlined />}>
+              <Menu.Item key="sales">
+                <Link to="/sales">Profile</Link>
+              </Menu.Item>
+              <Menu.Item key="sales-report">
+                <Link to="/sales-report">Settings</Link>
+              </Menu.Item>
+            </Menu.SubMenu>
+            <Menu.SubMenu key="sales" icon={<SendOutlined />}>
+              <Menu.Item key="History Global">
+                <Link to="/suppliers/history-supplier">History</Link>
+              </Menu.Item>
+              <Menu.Item key="calendar">
+                <Link to="/dispatch">Settings</Link>
+              </Menu.Item>
+            </Menu.SubMenu>
+
+            <Menu.SubMenu
+              key="accountSettings"
+              icon={<UserOutlined />}
+              title="Account Settings"
+              expandIcon={null}
             >
-                <Menu
-                    mode="inline"
-                    style={{ height: '100%', borderRight: 50 , paddingTop:'50px'}}
-                    defaultOpenKeys={['accountSettings']}
-                    expandIcon={null}
-                >
-                    <Menu.Item key="dashboard" icon={<FundProjectionScreenOutlined />}>
-                        <Link to="/dashboard">Dashboard</Link>
-                    </Menu.Item>
+              <Menu.Item key="profile">
+                <Link to="/profile">Profile</Link>
+              </Menu.Item>
+              <Menu.Item key="Settings">
+                <Link to="/Settings">Settings</Link>
+              </Menu.Item>
+              <Menu.Item key="changePassword">
+                <Link to="/change-password">Change Password</Link>
+              </Menu.Item>
+            </Menu.SubMenu>
 
-                    <Menu.Item key="orders" icon={<SolutionOutlined />}>
-                        <Link to="/orders">Orders</Link>
-                    </Menu.Item>
+            <Menu.SubMenu
+              key="Analytics"
+              icon={<FundOutlined />}
+              title="Analytics"
+              expandIcon={null}
+            >
+              <Menu.Item key="SalesGraphs">Level 1</Menu.Item>
+              <Menu.Item key="SalesGraphs">Level 2</Menu.Item>
+              <Menu.Item key="SalesGraphs">Level 3</Menu.Item>
+            </Menu.SubMenu>
 
-                    <Menu.Item key="reports" icon={<FormOutlined />}>
-                        <Link to="/reports">Reports</Link>
-                    </Menu.Item>
+            <Menu.SubMenu
+              key="warehouse"
+              icon={<TruckOutlined />}
+              title="Warehouse"
+              expandIcon={null}
+            >
+              <Menu.Item key="warehouse">
+                <Link to="/warehouse">Profile</Link>
+              </Menu.Item>
+              <Menu.Item key="inventory">
+                <Link to="/inventory">Inventory</Link>
+              </Menu.Item>
+              <Menu.Item key="suppliers">
+                <Link to="/suppliers">Suppliers</Link>
+              </Menu.Item>
+            </Menu.SubMenu>
 
-                    <Menu.SubMenu key="sales" icon={<ShoppingOutlined />}
-                        >
-                        <Menu.Item key="sales">
-                            <Link to="/sales">Profile</Link>
-                        </Menu.Item>
-                        <Menu.Item key="sales-report">
-                            <Link to="/sales-report">Settings</Link>
-                        </Menu.Item>
-                    </Menu.SubMenu>
+            <Menu.Divider />
 
-                    <Menu.SubMenu key="accountSettings" icon={<UserOutlined />} title="Account Settings" expandIcon={null}>
-                        <Menu.Item key="profile">
-                            <Link to="/profile">Profile</Link>
-                        </Menu.Item>
-                        <Menu.Item key="Settings">
-                            <Link to="/Settings">Settings</Link>
-                        </Menu.Item>
-                        <Menu.Item key="changePassword">
-                            <Link to="/change-password">Change Password</Link>
-                        </Menu.Item>
-                    </Menu.SubMenu>
+            <Menu.ItemGroup title="" expandIcon={null}>
+              <Menu.Item
+                key="important"
+                icon={<ExclamationCircleOutlined style={{ color: "red" }} />}
+              >
+                Important
+              </Menu.Item>
+              <Menu.Item
+                key="warning"
+                icon={<ExclamationCircleOutlined style={{ color: "orange" }} />}
+              >
+                Warning
+              </Menu.Item>
+              <Menu.Item
+                key="information"
+                icon={<ExclamationCircleOutlined style={{ color: "blue" }} />}
+              >
+                Information
+              </Menu.Item>
+            </Menu.ItemGroup>
+          </Menu>
+        </Sider>
 
-                    <Menu.SubMenu key="Analytics" icon={<FundOutlined />} title="Analytics"expandIcon={null}>
-                        <Menu.Item key="SalesGraphs">Level 1</Menu.Item>
-                        <Menu.Item key="SalesGraphs">Level 2</Menu.Item>
-                        <Menu.Item key="SalesGraphs">Level 3</Menu.Item>
-                    </Menu.SubMenu>
+        <Layout>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              padding: "10px 20px",
+              backgroundColor: "#f8fafc",
+              borderBottom: "1px solid #ddd",
+            }}
+          >
+            <Button
+              icon={<MenuFoldOutlined />}
+              shape="square"
+              onClick={() => setCollapsed(!collapsed)}
+            />
 
-                    <Menu.SubMenu key="warehouse" icon={<TruckOutlined />} title="Warehouse" expandIcon={null}>
-                        <Menu.Item key="warehouse">
-                            <Link to="/warehouse">Profile</Link>
-                        </Menu.Item>
-                        <Menu.Item key="inventory">
-                            <Link to="/inventory">Inventory</Link>
-                        </Menu.Item>
-                        <Menu.Item key="suppliers">
-                            <Link to="/suppliers">Suppliers</Link>
-                        </Menu.Item>
-                    </Menu.SubMenu>
+            {showSearch && (
+              <Input.Search
+                placeholder="Search"
+                style={{ marginLeft: "10px" }}
+              />
+            )}
 
-                    <Menu.Divider />
-
-                    <Menu.ItemGroup title="" expandIcon={null}>
-                        <Menu.Item key="important" icon={<ExclamationCircleOutlined style={{ color: 'red' }} />}>
-                            Important
-                        </Menu.Item>
-                        <Menu.Item key="warning" icon={<ExclamationCircleOutlined style={{ color: 'orange' }} />}>
-                            Warning
-                        </Menu.Item>
-                        <Menu.Item key="information" icon={<ExclamationCircleOutlined style={{ color: 'blue' }} />}>
-                            Information
-                        </Menu.Item>
-                    </Menu.ItemGroup>
-                </Menu>
-            </Sider>
-
-            <Layout>
-                <div style={{ display: 'flex', alignItems: 'center', padding: '10px 20px', backgroundColor: '#f8fafc', borderBottom: '1px solid #ddd' }}>
-                    <Button icon={<MenuFoldOutlined />} shape="square" onClick={() => setCollapsed(!collapsed)} />
-
-                    {showSearch && <Input.Search placeholder="Search" style={{ marginLeft: '10px' }} />}
-
-                    <div style={{ marginLeft: 'auto', display: 'flex', alignItems: 'center' }}>
-                        <Dropdown overlay={settingsMenu} placement="bottomLeft" trigger={['click']}>
-                            <Button icon={<UserOutlined />} shape="circle" style={{ marginLeft: '10px' }} />
-                        </Dropdown>
-                    </div>
-                </div>
-            </Layout>
+            <div
+              style={{
+                marginLeft: "auto",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <Dropdown
+                overlay={settingsMenu}
+                placement="bottomLeft"
+                trigger={["click"]}
+              >
+                <Button
+                  icon={<UserOutlined />}
+                  shape="circle"
+                  style={{ marginLeft: "10px" }}
+                />
+              </Dropdown>
+            </div>
+          </div>
         </Layout>
+      </Layout>
     );
 };
 
