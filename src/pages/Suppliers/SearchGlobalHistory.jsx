02b3990ecc95f8b060c84ca874c62f1add2b5974
AlexisPN <0321101363@ut-tijuana.edu.mx>
2024-11-24 21:22:28 -0800

    fusion main -> alexis

--- src/pages/Suppliers/SearchGlobalHistory.jsx
+++ src/pages/Suppliers/SearchGlobalHistory.jsx
@@ -18,24 +18,3 @@ SearchBar.propTypes = {
 };
 
 export default SearchBar;
-
-import { Input } from "antd";
-import PropTypes from "prop-types";
-
-const SearchBar = ({ onSearch }) => {
-  return (
-    <div>
-      <Input.Search
-        style={{ marginBottom: "20px", padding: "8px" }}
-        placeholder="Search by Supplier or Product Name..."
-        onChange={(e) => onSearch(e.target.value)}
-      />
-    </div>
-  );
-};
-
-SearchBar.propTypes = {
-  onSearch: PropTypes.func.isRequired,
-};
-
-export default SearchBar;
