35dea29d1afb059215733ce2ab825bf4579ea3cd
ElPyker <100382022+ElPyker@users.noreply.github.com>
2024-11-26 17:19:41 -0800

    Search ProductPage

--- src/pages/Inventory/ProductPage.jsx
+++ src/pages/Inventory/ProductPage.jsx
@@ -588,7 +588,7 @@ const ProductPage = () => {
         title="Products"
         buttonText="Add Product"
         onAddCategory={handleAdd}
-        onSearch={handleSearch}
+        onSearch={setSearchText}
       />
       <div
       style={{
